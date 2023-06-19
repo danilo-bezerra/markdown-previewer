974ca65a3e9637302dcfec2244295f28867729bf
Danilo Bezerra <86009742+danilo-bezerra@users.noreply.github.com>
2023-06-19 16:41:21 -0300

    remove ununsed imports

--- src/components/Sidebar/MarkdownItem.tsx
+++ src/components/Sidebar/MarkdownItem.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { IMarkdown } from "../../models/models";
 
 import { AiOutlineFileText } from "react-icons/ai";
