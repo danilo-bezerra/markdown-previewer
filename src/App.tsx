1885b66d08f345fd91367fab9cf96b6f09947832
Danilo Bezerra <86009742+danilo-bezerra@users.noreply.github.com>
2023-06-18 21:24:19 -0300

    add toggle markdown visibility

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,16 @@ function App() {
     <div className="bg-zinc-800 h-screen max-h-screen dark:text-zinc-100">
       <header className="h-20 bg-[#2b2d31]"></header>
       <main className="h-[calc(100vh-5rem)] max-h-[calc(100vh-5rem)]   ">
-        <section className="relative grid grid-cols-2  h-full ">
-          <div className="relative  overflow-y-hidden border-r border-r-gray-400">
+        <section
+          className={`relative grid   h-full ${
+            showMarkdown ? "grid-cols-2" : "grid-cols-1"
+          }`}
+        >
+          <div
+            className={`relative  overflow-y-hidden border-r border-r-gray-400 ${
+              showMarkdown ? "" : " hidden"
+            }`}
+          >
             <header className="sticky top-0 p-4  text-sm dark:bg-[#1d1f22] dark:text-gray-400 font-thin">
               MARKDOWN
             </header>
